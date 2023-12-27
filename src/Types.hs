54271a36fd00948ff89228fce36201048bbf93ab
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-12-27 19:43:21 +0000

    Made message type derive Eq for testing

--- src/Types.hs
+++ src/Types.hs
@@ -5,4 +5,8 @@ module Types where
 
 -- | A 'Message' represents a single message in the social network with its content.
 data Message = Message { messageContent :: String} 
+  deriving (Eq, Show)
+
+-- | A 'User' represents a user in the social network with a username and a list of received messages.
+data User = User { username :: String, messages :: [Message] }
   deriving (Show)
