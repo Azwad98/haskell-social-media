1ae5ca0def4192728b8ebd1b700404d90af2cf49
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-12-27 20:52:18 +0000

    Added main functionalities where 10 users are created and system is instructed to send 100 messages

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,39 @@
-module Main (main) where
+module Main where
+
+import Control.Concurrent
+import User
+import ConcurrentUtils
+import Types
 
 main :: IO ()
 main = do
-  putStrLn "Hello World"
+    -- Create users
+    aliceMVar <- createUser "Alice"
+    bobMVar <- createUser "Bob"
+    charlieMVar <- createUser "Charlie"
+    davidMVar <- createUser "David"
+    emmaMVar <- createUser "Emma"
+    frankMVar <- createUser "Frank"
+    graceMVar <- createUser "Grace"
+    henryMVar <- createUser "Henry"
+    ireneMVar <- createUser "Irene"
+    jackMVar <- createUser "Jack"
+    
+    -- Shared message counter
+    messageCounter <- newMVar 0
+
+    let users = [aliceMVar, bobMVar, charlieMVar, davidMVar, emmaMVar, frankMVar, graceMVar, henryMVar, ireneMVar, jackMVar]  -- List of all users
+    let messageLimit = 100
+
+    -- Start a thread for each user
+    mapM_ (\userMVar -> forkIO $ userThreadLimited userMVar (filter (/= userMVar) users) messageCounter messageLimit) users
+
+    -- Wait until 100 messages are sent
+    waitForMessages messageCounter messageLimit
+
+    putStrLn "Messages received by each user:"
+    mapM_ (\userMVar -> do
+        user <- readMVar userMVar
+        count <- messageCount userMVar
+        putStrLn $ username user ++ ": " ++ show count
+        ) users
